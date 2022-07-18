145e379dd772be0545a92d083632fd45a09a9e76
PedroAlencarbsi <pedro.alencar@edu.unirio.br>
2022-07-18 00:36:51 -0300

    Dashboard apagando evento

--- resources/views/events/dashboard.blade.php
+++ resources/views/events/dashboard.blade.php
@@ -5,8 +5,9 @@
 @section('content')
 
 <div class="col-md-10 offset-md-1 dashboard-title-container">
-<h1>Meus eventos</h1>
-
+    <h1>Meus eventos</h1>
+</div>
+<div class="col-md-10 offset-md-1 dashboard-events-container">
     @if(count($events) > 0)
 
     <table class="table">
@@ -24,7 +25,14 @@
                     <td scropt="row"> {{ $loop->index + 1 }}</td>
                     <td><a href="/events/{{ $event->id }}">{{ $event->title }}</a></td>
                     <td>0</td>
-                    <td><a href="#">Editar</a><a href="#">Deletar</a></td>
+                    <td>
+                        <a href="#" class="btn btn-info edit-btn"><ion-icon name="Create-outline"></ion-icon>Editar</a>
+                        <form action="/events/{{$event->id}}" method="POST">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="btn btn-danger delete-btn"><ion-icon name="trash-outline"></ion-icon>Deletar</button>
+                        </form>
+                    </td>    
                 </tr>
             @endforeach
         </tbody>
@@ -32,9 +40,6 @@
     @else
     <p>voce ainda nao tem eventos, <a href="/events/create">criar evento</a></p>
     @endif
-
-
 </div>
-
 
 @endsection
