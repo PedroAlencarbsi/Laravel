43222acd9f6e0325185a2f1833614ccd32601199
PedroAlencarbsi <pedro.alencar@edu.unirio.br>
2022-07-17 22:38:10 -0300

    first commit

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.main')
+
+@section('title', 'Dashboard')
+
+@section('content')
+
+<div class="col-md-10 offset-md-i dashboard-title-container">
+<h1>Meus eventos</h1>
+
+   
+
+
+</div>
+
+
+@endsection
